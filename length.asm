b9a05722f3e66da0204abe9eda7f4dc76364c45d
Varsha Shaheen <varshashaheen2003@gmail.com>
2024-01-03 18:07:02 +0530

    Create length.asm

---
+++ length.asm
@@ -0,0 +1,68 @@
+data segment
+msg1 db 13,10, "Enter first string $"
+msg2 db 13,10, "Enter second string $"
+msg3 db 13,10,"The concatenated string is: $"
+newline db 13,10
+buffer db 30 dup("$")
+data ends
+
+code segment
+assume ds:data, cs:code
+
+print_string proc
+mov ah,09h
+int 21h
+ret
+print_string endp
+
+read_char proc
+	mov ah,01h
+	int 21h
+	cmp al,13
+	ret
+read_char endp
+
+input_string proc
+	input:
+		call read_char
+		je exit_from_input
+		mov [si],al
+		inc si
+		jmp input
+	exit_from_input:
+		ret
+input_string endp
+
+start:
+mov ax,data
+mov ds,ax
+
+lea dx,msg1
+call print_string
+
+lea si,buffer
+call input_string
+
+lea si,buffer
+mov cl,0
+find_length:
+	cmp byte ptr [si],"$"
+	je exit_length
+	inc cl
+	inc si
+	jmp find_length
+exit_length:
+	add cl,30h
+	mov dl,cl
+	mov ah,02
+	int 21h
+
+lea dx,buffer
+call print_string
+	
+;exit
+mov ah,4ch
+int 21h
+
+code ends
+end start
